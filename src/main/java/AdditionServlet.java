fb90d1190406292bbc875aa2c2511b4a92ec8274
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-18 13:26:08 -0500

    Created the AdditionServlet file

--- src/main/java/AdditionServlet.java
+++ src/main/java/AdditionServlet.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class AdditionServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AdditionServlet", urlPatterns = "/add")
+public class AdditionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println(String.format("%d + %d = %d", num1, num2, (num1 + num2)));
+    }
 }
