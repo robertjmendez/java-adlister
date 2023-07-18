446726a8fe5720fac47dfd30a347ea8da2cc8ddb
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-18 13:24:48 -0500

    Created the CounterServlet file

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class CounterServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int count = 0;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        count++;
+
+        out.println("<h1>Page views: " + count + "</h1>");
+    }
 }
