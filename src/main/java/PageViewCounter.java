8cda97ed75e9a55f2442650c15162fd948f20518
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-18 13:29:32 -0500

    Created PageViewCounter file

--- src/main/java/PageViewCounter.java
+++ src/main/java/PageViewCounter.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/count")
-public class CounterServlet extends HttpServlet {
+public class PageViewCounter extends HttpServlet {
     private int count = 0;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
